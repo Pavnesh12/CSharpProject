99f71e92a32929555bb7f740b480c543cf505a0c
Pavnesh12 <pavneshprajapati898@gmail.com>
2021-11-24 22:02:23 +0530

    Employee wage problem

---
+++ EmployeepresentOrAbsent/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Employee check present or absent");
+int employeePresent = 1;
+Random random = new Random();
+int empcheck = random.Next(0, 2);
+Console.WriteLine(empcheck);
+if(empcheck == employeePresent)
+{
+    Console.WriteLine("Employee is present ");
+
+}
+else
+{
+    Console.WriteLine("Employee is absent");
+}
---
+++ calculateEmployeeWage/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Employee check present or absent");
+int employeePresent = 1;
+int empHour = 0;
+int empWagePerHour = 20;
+int empWage = 0;
+Random random = new Random();
+int empcheck = random.Next(0, 2);
+if (empcheck == employeePresent)
+{
+    Console.WriteLine("Employee is present");
+    empHour = 8;
+}
+else
+{
+    Console.WriteLine("Employee is absent");
+    empHour = 0;
+}
+empWage = empWagePerHour * empHour;
+Console.WriteLine("Employee wage is =" + empWage);
+
